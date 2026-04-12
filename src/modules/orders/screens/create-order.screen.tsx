23914159518d4ae2538ac3923ab9acd2ed77dd7f
dyersoftware <dyersoftware@gmail.com>
2026-04-12 18:57:47 +0530

    fix lint and build issue

--- src/modules/orders/screens/create-order.screen.tsx
+++ src/modules/orders/screens/create-order.screen.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function CreateOrderScreen() {
   return <div>CreateOrderScreen</div>;
 }
--- src/modules/payments/screens/payment-list.screen.tsx
+++ src/modules/payments/screens/payment-list.screen.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PaymentListComponent from "../components/payments-list.component";
 
 function PaymentListScreen() {
